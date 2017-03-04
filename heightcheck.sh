6fee5bf2f136a76438d5113a14746f3d23a49cc4
Mitchell <berry.mitch@gmail.com>
2017-03-05 02:19:31 +1100

    hedgetrimming

--- heightcheck.sh
+++ heightcheck.sh
@@ -1,29 +1,23 @@
 leader=$(factom-cli get heights | grep Leader | cut -c 15-)
 current=$(factom-cli get heights | grep Directory | cut -c 23-)
 difference=$(($leader - $current))
-
-#echo Current Height: $current
-#echo Leader Height: $leader
-#echo Difference: $difference
+#TODO: Restart if no response from cli
 
 if (($difference > 3))
   then
   sleep 10
   newcurrent=$(factom-cli get heights | grep Directory | cut -c 23-)
-  echo $(date -u) ": Daemon behind leader height, checking if stalled..." >> ~/restart.log
   if ((($newcurrent - $current) < 5 ))
     then
-    #restart
     pkill factomd
-    sleep 15
+    sleep 10
+    
+    #open factomd as a normal bg process
     factomd
-    #uncomment to run factomd in a detached screen session named fctd to allow later retrieval
+    
+    #open in a detached screen session named fctd for later retrieval
     #screen -dmS fctd factomd
+    
     echo $(date) ": Restarted factomd" >> ~/restart.log
-    else
-    echo $(date) ": False alarm, still syncing" >> ~/restart.log
  fi
 fi
-
-#api method
-#curl -X POST -H 'Content-Type: application/json' -d '{"params":{},"method":"heights","jsonrpc":"2.0", "id":"1"}' localhost:8088/v2
