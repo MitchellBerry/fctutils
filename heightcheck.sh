a28bf24f352d1e37b760a350eca03b4b5e608cba
Mitchell <berry.mitch@gmail.com>
2017-03-06 06:25:33 +1100

    simplified

--- heightcheck.sh
+++ heightcheck.sh
@@ -1,8 +1,6 @@
 leader=$(factom-cli get heights | grep Leader | cut -c 15-)
 current=$(factom-cli get heights | grep Directory | cut -c 23-)
 difference=$(($leader - $current))
-#TODO: Restart if no response from cli
-
 if (($difference > 3))
   then
   sleep 10
@@ -11,13 +9,8 @@ if (($difference > 3))
     then
     pkill factomd
     sleep 10
-    
-    #open factomd as a normal bg process
     factomd
-    
-    #open in a detached screen session named fctd for later retrieval
     #screen -dmS fctd factomd
-    
     echo $(date) ": Restarted factomd" >> ~/restart.log
  fi
 fi
