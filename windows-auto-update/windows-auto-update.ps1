197acb7a28e144739330175f4a0ea292f7f75cbc
Mitchell <berry.mitch@gmail.com>
2017-07-27 15:14:59 +1000

    formatting, neater info messages

--- windows-auto-update/windows-auto-update.ps1
+++ windows-auto-update/windows-auto-update.ps1
@@ -1,38 +1,45 @@
 $version = Read-Host "Enter Version Number For Binaries (eg. 4.2.4)"
 $url = "https://github.com/FactomProject/distribution/releases/download/v0.$version/FactomInstall-amd64.msi"
 $outfile = $PSScriptRoot + "FactomInstall-amd64.msi"
+$ProgressPreference="SilentlyContinue"
+
+function FatalError{
+    Write-Host $args[0]"Exiting."
+    Start-Sleep -s 5
+    Exit}
 
 try{
+    Write-Host "Checking for previous versions..."
     $app = Get-WmiObject -Class Win32_Product -Filter "Name = 'Factom'"
     $installedversion = $app.Version
     $guid = $app.IdentifyingNumber}
 catch{
-    Write-Host "No Previous Version Found"}
-
-function FatalError{
-    Write-Host $args[0]
-    Start-Sleep -s 3
-    Exit}
+    Write-Host "No Previous Version Found."}
 
 if ($version -eq $installedversion){
-    FatalError "Version number entered is already installed"}
+    FatalError "Version already installed."}
 
 try{
+    Write-Host "Downloading Version $version"
     Invoke-WebRequest $url -OutFile $outfile}
 catch{
-    FatalError "Version not found: $version. Exiting"}
+    FatalError "$url
+URL Not Found. Check Version Entered."}
 
 try{
-    Write-Host "Uninstalling previous version: $installedversion"
-    Start-Process -FilePath "msiexec.exe" -ArgumentList @("/x", $guid) -Wait}
-catch{
-    FatalError "Failed to Uninstall"}
+    Start-Process -FilePath "msiexec.exe" -ArgumentList @("/x", $guid) -Wait
+    Write-Host "Uninstalled previous version: $installedversion"}
+catch{"Nothing Uninstalled"}
 
 try{
     Write-Host "Installing..."
-    Start-Process $outfile -Wait
-    Write-Host "Removing installer file..."
-    Remove-Item $outfile
-    Write-host "All Done!"}
+    Start-Process $outfile -Wait}
 catch{
-    FatalError "Failed to install"}
+    FatalError "Failed to install."}
+
+try{
+    Remove-Item $outfile
+    Write-Host "Deleted installer file: $outfile"
+    FatalError "All Done!"}
+catch{
+    FatalError "Failed to remove installation file"}
