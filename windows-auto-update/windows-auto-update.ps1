9b4894bc1a187027179706c110c9ca41655e396f
Mitchell <berry.mitch@gmail.com>
2017-07-27 13:47:07 +1000

    Create windows-auto-update.ps1

---
+++ windows-auto-update/windows-auto-update.ps1
@@ -0,0 +1,37 @@
+$version = Read-Host "Enter Version Number For Binaries (eg. 4.2.4)"
+$url = "https://github.com/FactomProject/distribution/releases/download/v0.$version/FactomInstall-amd64.msi"
+$outfile = $PSScriptRoot + "FactomInstall-amd64.msi"
+
+try{
+    $app = Get-WmiObject -Class Win32_Product -Filter "Name = 'Factom'"
+    $installedversion = $app.Version}
+catch{
+    Write-Host "No Previous Version Found"}
+
+function FatalError{
+    Write-Host $args[0]
+    Start-Sleep -s 3
+    Exit}
+
+if ($version -eq $installedversion){
+    FatalError "Version number entered is already installed"}
+
+try{
+    Invoke-WebRequest $url -OutFile $outfile}
+catch{
+    FatalError "Version not found: $version. Exiting"}
+
+try{
+    Write-Host "Uninstalling previous version: $installedversion"
+    Start-Process -FilePath "msiexec.exe" -ArgumentList @("/x", $app.IdentifyingNumber) -Wait}
+catch{
+    FatalError "Failed to Uninstall"}
+
+try{
+    Write-Host "Installing..."
+    Start-Process $outfile -Wait
+    Write-Host "Removing installer file..."
+    Remove-Item $outfile
+    Write-host "Completed"}
+catch{
+    FatalError "Failed to install"}
