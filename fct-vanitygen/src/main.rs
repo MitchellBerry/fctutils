a4aa86939fa2f9a589cb10007e317a116d6586cd
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-06-03 21:15:25 +0800

    added fct-vanitygen

---
+++ fct-vanitygen/src/main.rs
@@ -0,0 +1,54 @@
+mod files;
+mod config;
+mod address;
+mod matching;
+
+use std::time;
+use files::*;
+use address::*;
+use matching::*;
+use config::parse_args;
+use rayon::ThreadPoolBuilder;
+
+fn main() {
+    // Build Configuration
+    let config = parse_args();
+    let names = read_file(&config.input);
+    let rcd = if config.ec {ec_rcd} else {fct_rcd};
+    let mut keys_file = initialise_output_file(&config.output);
+    let set = compile_regex(names, config.case, &config.regex_prefix);
+    let pool = ThreadPoolBuilder::new()
+                .num_threads(config.threads as usize).build().unwrap();
+    
+    // Rudimentary rate checking
+    let mut count: u64 = 0;
+    let mut start = time::Instant::now();
+
+    // Main threadpool loop
+    loop {
+        pool.install(|| {
+            let keypair = generate_ed25519_keypair();
+            let pub_address = readable(&config.pub_prefix,
+                                        &rcd(keypair.public.to_bytes()));
+            if set.is_match(&pub_address){
+                let priv_address = readable(&config.priv_prefix,
+                                            &keypair.secret.to_bytes());
+                write_keys(&mut keys_file, &pub_address, &priv_address);
+                if config.verbose {
+                    println!("Public Address: {}\nPrivate Address: {}\n",
+                            pub_address, priv_address);
+                }
+            }
+        });
+
+        // Rudimentary rate checking
+        count += 1;
+        if count == 100000 {
+            let time = start.elapsed().as_secs();
+            let rate = count / time;
+            println!("{} addresses per second\n", rate);
+            count = 0;
+            start = time::Instant::now()
+        }
+    }
+}
